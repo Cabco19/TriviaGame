51344ea524395b47ea9391e75e5c63c94ef00ada
Ceara Owre <cearaowre@hotmail.com>
2019-10-11 18:28:53 -0400

    CSS Updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 window.onload = function () {
   $("#start").on("click", start);
   $("#trivia").css("display", "none");
-  $("#reset").css("display", "none");
 };
 
 //  Global variables
 var intervalId;
-var time = 10;
+var time = 60;
 var clockRunning = false;
 var wins = 0;
 var losses = 0;
@@ -15,11 +14,11 @@ var none = 10;
 function start() {
   // REMEMBER TO REVISE TIMER BEFORE FINAL UPLOAD
   //  Start the timer. Use setInterval to start the count here and set the clock to running. Using setTimeout stop the clock after REVISE THIS NUMBER WHEN DONE30seconds
-  time = 10;
+  time = 60;
   wins = 0;
   losses = 0;
   none = 10;
-  $("#timer").text("Time Remaining: 30 Seconds")
+  $("#timer").html("Time Remaining &ndash; 60 Seconds")
   $("#timesUp").empty();
   $("#correctAns").empty();
   $("#incorrectAns").empty();
@@ -29,7 +28,7 @@ function start() {
   $("#start").css("display", "none");
 
   if (!clockRunning) {
-    setTimeout(stop, 1000 * 11);
+    setTimeout(stop, 1000 * 61);
     // for (var i = 0; i < 10; i++) {
     intervalId = setInterval(count, 1000);
     clockRunning = true;
@@ -46,36 +45,20 @@ function stop() {
   $("#incorrectAns").text("Incorrect Answers:  " + losses);
   $("#unAnswered").text("Unanswered:  " + none);
   $("#start").css("display", "inline");
-  $("#start").text("Play Again?");
-
+  $("#start").text("Play Again");
 
   // Hide questions after clock times out
   $("#trivia").css("display", "none");
-
-  $("#reset").css("display", "inline", start);
-
-
 }
 
 function count() {
   time--;
   if (time > 9) {
-    $("#timer").text("Time Remaining: " + time + " Seconds");
+    $("#timer").html("Time Remaining &ndash; " + time + " Seconds");
   }
   if (time <= 9) {
-    $("#timer").html("Time Remaining:&nbsp;&nbsp;&nbsp;" + time + "  Seconds");
+    $("#timer").html("Time Remaining&nbsp;&nbsp;&ndash;&nbsp;&nbsp;" + time + "  Seconds");
   }
-}
-
-function reset() {
-  time = 30;
-  wins = 0;
-  losses = 0;
-  none = 10;
-  //  Changes the "display" div to "30 seconds" when game restarts
-  $("#display").text("40 Seconds");
-  $("#trivia").css("display", "inline");
-  $("#start").on("click", start);
 }
 
 function answerLog() {
